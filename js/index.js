c718f706b654779f736f99f5eb4ab51415535f70
Taylor <taylorhahn27@gmail.com>
2020-05-03 18:55:38 -0400

    test

--- js/index.js
+++ js/index.js
@@ -9,6 +9,30 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+function menu() {
+  window.open("images/SFC takeout menu.jpg");
+}
+
+$(document).ready(function () {
+  var show_btn = $(".show-modal");
+  var show_btn = $(".show-modal");
+  //$("#testmodal").modal('show');
+
+  show_btn.click(function () {
+    $("#testmodal").modal("show");
+  });
+});
+
+$(function () {
+  $("#element").on("click", function (e) {
+    Custombox.open({
+      target: "#testmodal-1",
+      effect: "fadein",
+    });
+    e.preventDefault();
+  });
+});
+
 $(document).ready(function () {
   $("#myModal").modal("show");
 });
